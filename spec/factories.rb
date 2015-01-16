fd1e0333d0a182633342ae5563bcc6d5c71ba578
sacasher <sacasher@gmail.com>
2015-01-16 01:03:54 -0600

    Finish the signup form

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name "Saul"
+    email "sacasher@gmail.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -17,4 +17,50 @@ RSpec.describe "UserPages", :type => :feature do
     it {expect(page).to have_selector('h1',text: 'Sign up')}
     it {expect(page).to have_title full_title 'Sign up'}
   end
+  
+  describe "profile page" do
+    let(:foobar) { FactoryGirl.create :user}
+    before {visit user_path foobar}
+
+    it {expect(page).to have_selector('h1',text: foobar.name)}
+    it {expect(page).to have_title full_title foobar.name}
+  end
+
+  describe "signup" do
+    before{ visit signup_path }
+    let(:submit) {"Create my account"}
+    describe "With invalid information" do
+      it "Should not create a user" do
+        expect {click_button submit}.not_to change(User,:count)
+      end
+
+      describe "after submission" do
+        before{click_button submit}
+        it{expect(page).to have_title "Sign up"}
+        it{expect(page).to have_content "error"}
+        it{expect(page).not_to have_content "Password digest"}
+      end
+    end
+
+    describe "With valid information" do
+      before do
+        fill_in "Name", with: "Example User"
+        fill_in "Email", with: "user@example.com"
+        fill_in "Password", with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+
+      it "Should create a user" do
+        expect {click_button submit}.to change(User,:count).by 1
+      end
+
+      describe "after saving a user" do
+        before{click_button submit}
+        let(:user) {User.find_by_email("user@example.com")}
+        it{expect(page).to have_title user.name}
+        it{expect(page).to have_selector("div.alert.alert-success", text: "Welcome")}
+      end
+    end
+
+  end
 end
